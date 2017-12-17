f9bd103a37ad72a3463e83d6d5acee86987d6fba
Vadim Eisenberg <vadime@il.ibm.com>
2017-12-17 21:18:43 +0200

    updated Kitura dependency - set it to 4.0

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Todo-Backend-InMemory",
     dependencies: [
-      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMajor(from: "2.0.0")),
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.0.0")),
       .package(url: "https://github.com/vadimeisenbergibm/Todo-Backend-InMemory-DataLayer.git",
                    .upToNextMajor(from: "0.0.0")),
       .package(url: "https://github.com/vadimeisenbergibm/Todo-Backend-Router.git",
